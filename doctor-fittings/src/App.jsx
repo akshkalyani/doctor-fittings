c4d7b1cf867d16e55c977f8f7136e90f157659ce
akshkalyani <akshkalyani@gmail.com>
2026-05-04 17:42:03 +0530

    [V2] : Added address route

--- doctor-fittings/src/App.jsx
+++ doctor-fittings/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         <Route
           path="/address"
           element={
-            <ExternalRedirect url="https://www.google.com/maps/place/Doctor+Fittings+%26+Hardware/@22.2382851,70.80016,17z/data=!3m1!4b1!4m6!3m5!1s0x3959cb94e59cf4a7:0x2042dbca35b450dd!8m2!3d22.2382851!4d70.8027349!16s%2Fg%2F11k6963kw7?entry=ttu&g_ep=EgoyMDI2MDQyOS4wIKXMDSoASAFQAw%3D%3D" />
+            <ExternalRedirect url="https://maps.app.goo.gl/TDe9t73EsMqpz8gw9" />
           }
         />
       </Routes>
